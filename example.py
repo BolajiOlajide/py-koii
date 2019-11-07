318a24c8e04b2581f102326a749d8423e00ae2cc
Bolaji Olajide <cooproton@gmail.com>
2019-11-08 00:06:27 +0100

    add example flask app

---
+++ example.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+
+from koii import Koii
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({"status": "success"})
+
+
+@app.route("/proton", methods=['GET', 'PATCH'])
+def proton():
+    return jsonify({"status": "success", "route": "proton"})
+
+
+Koii(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
