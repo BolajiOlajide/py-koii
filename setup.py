48d8821c09301096a8a11ae7a907da37f880c4ad
Bolaji Olajide <cooproton@gmail.com>
2019-11-08 02:00:45 +0100

    add support for py2 and py3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(current_path, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="koii",
-    version="1.0.0",
+    version="1.0.1",
     description="A python library to display routes in a flask application",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -32,8 +32,11 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     packages=["koii"],
     include_package_data=True,
